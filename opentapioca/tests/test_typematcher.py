e0ee4b8de96db4013fa221eb2c1fc2b245a8e7dd
Antonin Delpeuch <antonin@delpeuch.eu>
2019-02-06 15:10:51 +0000

    Improve tests for TypeMatcher

--- opentapioca/tests/test_typematcher.py
+++ opentapioca/tests/test_typematcher.py
@@ -6,6 +6,9 @@ from opentapioca.typematcher import TypeMatcher
 from .test_fixtures import cache_requests
 
 def test_typematcher(cache_requests):
+    """
+    Tests the caching of the type hierarchy, by parent.
+    """
     t = TypeMatcher()
     # First query (cached by cache_requests
     # any university (Q3918) is an organization (Q43229)
@@ -14,4 +17,12 @@ def test_typematcher(cache_requests):
     # Second query disabled - still works because of local caching
     with requests_mock.Mocker(real_http=True) as mocker:
         mocker.get(re.compile('.*'), status_code=500)
-        assert t.is_subclass('Q3918', 'Q43229')
+        # a foundation (Q157031) is an organization (Q43229)
+        assert t.is_subclass('Q157031', 'Q43229')
+        
+def test_reflexive(cache_requests):
+    """
+    Any type is a subclass of itself
+    """
+    t = TypeMatcher()
+    assert t.is_subclass('Q43229', 'Q43229')
