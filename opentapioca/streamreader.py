647731ec0d46329f08956d81a366c511136c3e40
Antonin Delpeuch <antonin@delpeuch.eu>
2019-04-02 16:05:26 +0100

    Introduce stream reader to stay in sync

---
+++ opentapioca/streamreader.py
@@ -0,0 +1,90 @@
+import json
+import requests
+import requests.exceptions
+from opentapioca.wditem import WikidataItemDocument
+from time import sleep
+
+from sseclient import SSEClient
+
+class WikidataStreamReader(object):
+    """
+    Generates a stream of `WikidataItemDocument` from
+    the Wikidata edit stream.
+    """
+    
+    def __init__(self,
+                 endpoint='https://stream.wikimedia.org/v2/stream/recentchange',
+                 wiki='wikidatawiki',
+                 mediawiki_api='https://www.wikidata.org/w/api.php',
+                 from_time=None):
+        self.endpoint = endpoint
+        self.wiki = wiki
+        self.mediawiki_api = mediawiki_api
+        self.from_time = from_time
+        self.stream = None
+        self.item_buffer = []
+        self.batch_size = 50
+        self.namespaces = [0]
+        self.retries = 5
+        self.delay = 5
+
+    def __enter__(self):
+        url = self.endpoint
+        if self.from_time is not None:
+             url += '?since='+from_time.isoformat().replace('+00:00', 'Z')
+        self.stream = SSEClient(url)
+        return self
+    
+    def __exit__(self, *args, **kwargs):
+        return None
+
+    def __iter__(self):
+        if not self.stream:
+            raise ValueError('Stream has not been started.')
+        stream_ended = False
+        while not stream_ended:
+            # Fetch new batch of events
+            qids = [self.fetch_next_qid() for _ in range(self.batch_size)]
+            
+            stream_ended = None in qids
+            qids_without_none = {qid for qid in qids if qid}
+            
+            # Fetch item contents
+            for item in self.fetch_items(qids_without_none):
+                yield item
+    
+    def fetch_next_qid(self):
+        """
+        Fetches the next Qid in the Wikidata edit stream
+        """
+        for event in self.stream:
+            if event.event == 'message':
+                try:
+                    change = json.loads(event.data)
+                    if (change.get('wiki') == self.wiki and
+                        change.get('namespace') in self.namespaces and
+                        change.get('title')):
+                        return change['title']
+                except ValueError:
+                    pass
+                
+    def fetch_items(self, qids):
+        """
+        Given a list of qids, fetch the corresponding documents via the Wikidata API.
+        """
+        for retries in range(self.retries):
+            try:
+                req = requests.get(self.mediawiki_api, {
+                    'format':'json',
+                    'action':'wbgetentities',
+                    'ids':'|'.join(qids)})
+                req.raise_for_status()
+                result = req.json().get('entities').values()
+                return [WikidataItemDocument(payload) for payload in result if 'missing' not in payload]
+            except (requests.exceptions.RequestException, ValueError):
+                if retries < self.retries-1:
+                    sleep((1+retries)*self.delay)
+                else:
+                    raise
+
+
---
+++ opentapioca/tests/test_streamreader.py
@@ -0,0 +1,89 @@
+import os
+import pytest
+import requests.exceptions
+import requests_mock
+import json
+
+from pytest_mock import mocker
+from opentapioca.streamreader import WikidataStreamReader
+from collections import namedtuple
+from .test_fixtures import testdir
+from opentapioca.wditem import WikidataItemDocument
+
+EventStubBase = namedtuple('EventStubBase', ['data', 'event'])
+
+
+def EventStub(event='message', wiki='wikidatawiki', namespace=0, title='Q123'):
+    return EventStubBase(event=event, data=json.dumps(
+            {'wiki':wiki, 'namespace':namespace, 'title':title}
+            ))
+
+
+@pytest.fixture
+def wbgetentities_response(testdir):
+    with open(os.path.join(testdir, 'data', 'wbgetentities_response.json'), 'r') as f:
+        return f.read()
+
+    
+@pytest.fixture
+def event_stream():
+    return [
+        EventStub(event='something happened'),
+        EventStub(title='Q123'),
+        EventStub(namespace=2),
+        EventStub(title='Q456'),
+        EventStub(title='Q789'),
+    ]
+
+
+def test_fetch_items(wbgetentities_response):
+    reader = WikidataStreamReader()
+    with requests_mock.mock() as mocker:
+        mocker.get('https://www.wikidata.org/w/api.php?format=json&action=wbgetentities&ids=Q123%7CQ456%7CQ789', text=wbgetentities_response)
+        
+        items = reader.fetch_items(['Q123', 'Q456', 'Q789'])
+        
+        assert len(items) == 2  # Q789 does not exist
+        assert items[0].get_types() == ['Q47018901']
+        
+        
+def test_fetch_items_500_error(wbgetentities_response):
+    reader = WikidataStreamReader()
+    reader.delay = 0
+    with requests_mock.mock() as mocker:
+        mocker.get('https://www.wikidata.org/w/api.php?format=json&action=wbgetentities&ids=Q123%7CQ456%7CQ789', status_code=500)
+        
+        with pytest.raises(requests.exceptions.RequestException):
+            items = reader.fetch_items(['Q123', 'Q456', 'Q789'])
+
+            
+class StreamReaderStub(WikidataStreamReader):
+
+    def __init__(self, events):
+        super(StreamReaderStub, self).__init__()
+        self.stub_events = events
+        
+    def __enter__(self):
+
+        def generate():
+            for event in self.stub_events:
+                yield event
+
+        self.stream = generate()
+        return self
+    
+            
+def test_iterate(event_stream, wbgetentities_response, mocker):
+    reader = StreamReaderStub(event_stream)
+    method = mocker.patch.object(reader, 'fetch_items')
+    method.return_value = [WikidataItemDocument({'id':'Q123'}), WikidataItemDocument({'id':'Q456'})]
+    
+    with reader as entered_reader:
+        items = list(entered_reader)
+        
+        method.assert_called_with({'Q123', 'Q456', 'Q789'})
+        method.assert_called_once()
+        assert [item.get('id') for item in items] == ['Q123', 'Q456']
+        
+            
+        
