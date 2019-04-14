ae3490fe5354747631d46a9e6d248803af621d60
Antonin Delpeuch <antonin@delpeuch.eu>
2019-04-14 20:44:02 +0100

    Do not load pretrained data during testing

---
+++ settings_travis.py
@@ -0,0 +1,10 @@
+
+# The name of the Solr collection where Wikidata is indexed
+SOLR_COLLECTION = None
+
+# The path to the language model, trained with "tapioca train-bow"
+LANGUAGE_MODEL_PATH=None
+# The path to the pagerank Numpy vector, computed with "tapioca compute-pagerank"
+PAGERANK_PATH=None
+# The path to the trained classifier, obtained from "tapioca train-classifier"
+CLASSIFIER_PATH=None
